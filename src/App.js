6b77a24b7431b9fc70f06865f27b8f876244e9d5
Dushyant <56475253+dushyant615@users.noreply.github.com>
2023-06-17 12:56:27 +0000

    added nested routing using react router 'outlet'

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
 import Home from "./routes/home/home.component";
 
-const App = ()=> {
+const Navigation = () =>{
+  return(
+    <div>
+      <div>
+        <h1>I am navigation bar</h1>
+      </div>
+      <Outlet/>
+    </div>
+  )
+}
 
+const Shop = () =>{
+  return <h1>I am the shop page</h1>
+}
+
+const App = ()=> {
+  // below we have implemented nested routing, the <outlet/> 
+  // in main navigtion component will be replaced by home and shop 
+  // page when its path is matched. eg:- /home, /shop
   return (
     <Routes>
-      <Route path="/home" element={<Home/>}/>
-      
+      <Route path="/" element={<Navigation/>}>
+        {/* index below allows to set it as a base component of <Navigation/>, when only slash'/' is there*/}
+        <Route index={true} element={<Home/>}/> 
+        <Route path="shop" element={<Shop/>}/>
+      </Route>      
     </Routes>
   )
 }
