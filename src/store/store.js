8238aa00ec44b091d54f37167e6935ff45a01bf0
Dushyant <56475253+dushyant615@users.noreply.github.com>
2023-11-30 20:08:51 +0000

    added redux-persist for localstorage.(end section15)

--- src/store/store.js
+++ src/store/store.js
@@ -1,24 +1,28 @@
 // this is our redux store
 import {compose, legacy_createStore as createStore, applyMiddleware} from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; //saves store state into local storage
+// import storageSession from 'redux-persist/lib/storage/session'; //save store state into session storage
 import logger from 'redux-logger';
 import { rootReducer } from './root-reducer';
 
+const persistConfig = {
+    key: 'root',
+    storage: storage,
+    blacklist: ['user']
+}
+
+const persistedReducer = persistReducer(persistConfig,rootReducer);
+
 // middlewares catches actions before the reducers are hit.
-const middleWares = [logger]; //we can add multiple middlewares here
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(Boolean); //we can add multiple middlewares here
+const composedEnhancer =
+    (process.env.NODE_ENV !== 'production' && 
+        window &&
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+    compose;
+const composedEnhancers = composedEnhancer(applyMiddleware(...middleWares));
 
 // we need root reducer in order to generate the store.
-export const store = createStore(rootReducer, undefined, composedEnhancers);
-
-// custom redux logger
-// const loggerMiddleware = (store) => (next) => (action) => {
-//     if(!action.type){
-//         return next(action);
-//     }
-//     console.log('type: ', action.type);
-//     console.log('payload: ', action.payload);
-//     console.log('currentState: ', store.getState());
-
-//     next(action);
-//     console.log('next state: ',store.getState());
-// }
+export const store = createStore(persistedReducer, undefined, composedEnhancers);
+export const persistor = persistStore(store);
